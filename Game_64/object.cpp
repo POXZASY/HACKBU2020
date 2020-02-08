25b7457a3bedafa7c3db4e86105ea0f3aa3ef8ac
Thomas Horowitz <thorowi1@binghamton.edu>
2020-02-08 14:51:13 -0500

    edit to make file

---
+++ Game_64/object.cpp
@@ -0,0 +1,6 @@
+#include "object.h"
+
+int main(){
+  return 0;
+  Object object = Object()
+}
---
+++ Game_64/object.h
@@ -0,0 +1,18 @@
+#ifndef OBJECT
+#define OBJECT
+
+#include <cstdlib>
+#include <iostream>
+
+class Object{
+  int x;
+  int y;
+  int z;
+  int xcolor[3];
+  int ycolor[3];
+  int zcolor[3];
+  obect(int, int, int, int[], int[], int[]);
+
+};
+
+#endif
